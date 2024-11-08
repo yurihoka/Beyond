1f1402407f537a7ad0afcf8163e68e1e02ccce77
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2024-11-07 16:32:15 -0800

    add: signup APIテスト

---
+++ src/app/api/signup/route.test.ts
@@ -0,0 +1,37 @@
+import { NextApiRequest } from "next";
+import { createMocks } from "node-mocks-http";
+
+import POST from "./route";
+
+describe("API: Login", () => {
+  describe("サインアップ成功", () => {
+    test("", async () => {
+      const newUser = { email: "newuser@xyz.com", password: "newuser" };
+      const { req }: { req: NextApiRequest } = createMocks({
+        method: "POST",
+        body: newUser,
+        headers: { "Content-Type": "application/json" },
+      });
+      const response = await POST(req);
+
+      expect(response.status).toBe(200);
+    });
+  });
+
+  describe("サインアップ失敗", () => {
+    test("既登録ユーザーあり", async () => {
+      const existentUser = {
+        email: "existentuser@xyz.com",
+        password: "existentuser",
+      };
+      const { req }: { req: NextApiRequest } = createMocks({
+        method: "POST",
+        body: existentUser,
+        headers: { "Content-Type": "application/json" },
+      });
+      const response = await POST(req);
+
+      expect(response.status).toBe(401);
+    });
+  });
+});
