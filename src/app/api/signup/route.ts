ce6da3de211a4702cdb43c5267bc5f6d131ae989
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2024-11-13 17:10:06 -0800

    update: サインアップAPI DB接続

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,11 +1,25 @@
+import { createClient } from "@supabase/supabase-js";
 import { NextApiRequest } from "next";
 
-export default async function POST(req: NextApiRequest) {
-  const { email } = req.body;
+export async function POST(req: NextApiRequest) {
+  const { email, password } = req.body;
 
-  // 以下は全てDB構築するまでの一時的な認証処理
-  if (email === "existentuser@xyz.com") {
+  console.log(email, password);
+
+  const supabase = createClient(
+    process.env.SUPABASE_URL as string,
+    process.env.SUPABASE_ANON_KEY as string
+  );
+
+  const { data, error } = await supabase
+    .from("users")
+    .insert({ email: email, password: password });
+
+  if (error?.details.includes("already exists")) {
     return new Response("Emailが既に使われています", { status: 401 });
+  } else if (error) {
+    console.log(error);
+    return new Response("サーバーエラー", { status: 500 });
   }
   return new Response("サインアップ成功", { status: 200 });
 }
