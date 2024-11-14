6cd7b88350ba855c3c4cb114259154c55808a422
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2024-11-14 13:31:16 -0800

    [Feat] サインアップAPI: DB接続 (#47)
    * add: サインアップAPI テスト
    
    * update: サインアップAPI DB接続
    
    * chore: 不要なconsole.logを削除
    
    * update: サインアップAPI エラー処理追加

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,11 +1,29 @@
+import { createClient } from "@supabase/supabase-js";
 import { NextApiRequest } from "next";
 
-export default async function POST(req: NextApiRequest) {
-  const { email } = req.body;
+export async function POST(req: NextApiRequest) {
+  const { email, password } = req.body;
 
-  // 以下は全てDB構築するまでの一時的な認証処理
-  if (email === "existentuser@xyz.com") {
-    return new Response("Emailが既に使われています", { status: 401 });
+  try {
+    const supabase = createClient(
+      process.env.SUPABASE_URL as string,
+      process.env.SUPABASE_ANON_KEY as string
+    );
+
+    const { data, error } = await supabase
+      .from("users")
+      .insert({ email: email, password: password });
+
+    if (error?.details.includes("already exists")) {
+      return new Response("Emailが既に使われています", { status: 401 });
+    }
+    if (error) {
+      console.log(error);
+      return new Response("サーバーエラー", { status: 500 });
+    }
+    return new Response("サインアップ成功", { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return new Response("サーバーエラー", { status: 500 });
   }
-  return new Response("サインアップ成功", { status: 200 });
 }
