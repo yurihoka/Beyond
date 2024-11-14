61908082eb70195916cf99f9abcc8d7d9cfd0fcd
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2024-11-13 17:23:45 -0800

    update: サインアップAPI エラー処理追加

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -4,20 +4,26 @@ import { NextApiRequest } from "next";
 export async function POST(req: NextApiRequest) {
   const { email, password } = req.body;
 
-  const supabase = createClient(
-    process.env.SUPABASE_URL as string,
-    process.env.SUPABASE_ANON_KEY as string
-  );
+  try {
+    const supabase = createClient(
+      process.env.SUPABASE_URL as string,
+      process.env.SUPABASE_ANON_KEY as string
+    );
 
-  const { data, error } = await supabase
-    .from("users")
-    .insert({ email: email, password: password });
+    const { data, error } = await supabase
+      .from("users")
+      .insert({ email: email, password: password });
 
-  if (error?.details.includes("already exists")) {
-    return new Response("Emailが既に使われています", { status: 401 });
-  } else if (error) {
-    console.log(error);
+    if (error?.details.includes("already exists")) {
+      return new Response("Emailが既に使われています", { status: 401 });
+    }
+    if (error) {
+      console.log(error);
+      return new Response("サーバーエラー", { status: 500 });
+    }
+    return new Response("サインアップ成功", { status: 200 });
+  } catch (err) {
+    console.log(err);
     return new Response("サーバーエラー", { status: 500 });
   }
-  return new Response("サインアップ成功", { status: 200 });
 }
