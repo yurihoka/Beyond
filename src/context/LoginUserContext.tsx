2618f3473a8766b67f999fd4d74bf0b29cb00c90
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2024-11-14 18:07:56 -0800

    add: ログインユーザー用のcontext作成

---
+++ src/context/LoginUserContext.tsx
@@ -0,0 +1,27 @@
+import {
+  createContext,
+  type Dispatch,
+  type SetStateAction,
+  useContext,
+  useState,
+} from "react";
+
+type LoginUser = { email: string; password: string } | null;
+type LoginUserContextType =
+  | [LoginUser, Dispatch<SetStateAction<LoginUser>>]
+  | null;
+
+const LoginUserContext = createContext<LoginUserContextType>(null);
+export const LoginUserProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [loginUser, setLoginUser] = useState<LoginUser>(null);
+
+  return (
+    <LoginUserContext.Provider value={[loginUser, setLoginUser]}>
+      {children}
+    </LoginUserContext.Provider>
+  );
+};
