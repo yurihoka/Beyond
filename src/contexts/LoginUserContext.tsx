f0b90da9f6c39825facc0ae60af2c14be4c3a730
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2024-11-14 18:50:46 -0800

    chore: ディレクトリ名を複数形に変更

---
+++ src/contexts/LoginUserContext.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import {
+  createContext,
+  type Dispatch,
+  type SetStateAction,
+  useContext,
+  useState,
+} from "react";
+
+type LoginUser = { email: string; password: string } | null;
+export type LoginUserContextType = {
+  loginUser: LoginUser;
+  setLoginUser: Dispatch<SetStateAction<LoginUser>>;
+};
+
+export const LoginUserContext = createContext<LoginUserContextType>({
+  loginUser: null,
+  setLoginUser: () => {},
+});
+export const LoginUserProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [loginUser, setLoginUser] = useState<LoginUser>(null);
+
+  return (
+    <LoginUserContext.Provider value={{ loginUser, setLoginUser }}>
+      {children}
+    </LoginUserContext.Provider>
+  );
+};
