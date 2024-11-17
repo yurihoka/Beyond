9b2babf0e9a9dd55ce4c318adb6a9d8f9d3764d3
Yurika Hokama <125883144+yurihoka@users.noreply.github.com>
2024-11-16 16:51:08 -0800

    [UI] Workoutコンポーネント作成 (#48)
    * remove: switchは不要なため削除
    
    * [Clean] 条件式の書き換え
    
    * [Feat] add setボタン追加
    
    * [Feat] Add exercisesボタン追加
    
    * [Feat] cancel workoutボタン追加
    
    * [Fix] ディレクトリの場所修正
    
    * [Fix] ディレクトリの場所修正
    
    * [Update] 引数追加
    
    * [Update] デザインをテーブルように変更
    
    * [Feat] Exerciseコンポーネント作成
    
    * [Update] ボタンの幅変更
    
    * [Remove] args不要のため削除
    
    * [Update] Buttonコンポーネントの位置変更
    
    * [Update] props変更
    
    * [Feat] WorkoutRecordコンポーネントを追加する機能追加
    
    * [Update] ディレクトリ名変更
    
    * [UI] workoutコンポーネント作成
    
    ---------
    
    Co-authored-by: Hidemichi Shimura <hidemichi.shimura@gmail.com>

---
+++ src/components/workout/Workout.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "../common/Navbar";
+import Button from "../common/Button";
+import Exercise from "./Exercise";
+
+const Workout = () => {
+  const [currentDate, setCurrentDate] = useState<string>("");
+
+  useEffect(() => {
+    const date = new Date();
+    setCurrentDate(date.toLocaleDateString());
+  }, []);
+
+  return (
+    <div className="relative">
+      <div className="fixed top-0 left-0 w-full">
+        <Navbar userEmail="test@xyz.com" />
+      </div>
+      <div className="flex flex-col pt-16">
+        <h1>{currentDate}</h1>
+        <Button entry="finishworkout" />
+      </div>
+      <Exercise />
+      <Button entry="addexercises" />
+      <Button entry="cancelworkout" />
+    </div>
+  );
+};
+
+export default Workout;
---
+++ src/components/workout/WorkoutRecord.stories.ts
@@ -0,0 +1,28 @@
+import { Meta } from "@storybook/react";
+import WorkoutRecord from "./WorkoutRecord";
+
+const meta: Meta<typeof WorkoutRecord> = {
+  title: "Workout/WorkoutRecord",
+  component: WorkoutRecord,
+  tags: ["autodocs"],
+  parameters: {
+    docs: {
+      description: {
+        component: "エクササイズの名前、回数、重さを1セットごとに記録する",
+      },
+    },
+  },
+  argTypes: {
+    set: {
+      description: "エクササイズごとのセット数",
+    },
+  },
+};
+
+export default meta;
+
+export const SingleRecord = {
+  args: {
+    set: 1,
+  },
+};
