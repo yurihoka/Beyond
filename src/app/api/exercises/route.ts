d02124d8a6e50788a071ca31d5816c301daf5bd5
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2024-12-04 18:14:21 +0900

    add: /exercise GET DB連携

--- src/app/api/exercises/route.ts
+++ src/app/api/exercises/route.ts
@@ -1,1 +1,19 @@
-export async function GET() {}
+import { createClient } from "@supabase/supabase-js";
+
+export async function GET() {
+  try {
+    const client = createClient(
+      process.env.SUPABASE_URL as string,
+      process.env.SUPABASE_ANON_KEY as string
+    );
+    const { data, error } = await client.from("exercises").select("name");
+
+    if (error) {
+      throw error;
+    }
+    return new Response(JSON.stringify(data));
+  } catch (err) {
+    console.log(err);
+    return new Response("サーバーエラーが発生しました", { status: 500 });
+  }
+}
