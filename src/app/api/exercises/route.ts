5605919140f4bf328b8593d36129c3a22efe2d89
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2024-12-05 14:15:30 +0900

    add: /exercises API トレーニング種目更新

--- src/app/api/exercises/route.ts
+++ src/app/api/exercises/route.ts
@@ -40,3 +40,32 @@ export async function POST(req: NextApiRequest) {
     return new Response("サーバーエラーが発生しました", { status: 500 });
   }
 }
+
+export async function PATCH(req: NextApiRequest) {
+  try {
+    const client = createClient(
+      process.env.SUPABASE_URL as string,
+      process.env.SUPABASE_ANON_KEY as string
+    );
+    const { data, error } = await client
+      .from("exercises")
+      .update({ name: req.body.name })
+      .eq("id", req.body.id)
+      .select();
+
+    if (error?.code === "23505")
+      return new Response("既に存在するトレーニング種目です", {
+        status: 409,
+      });
+    if (data?.length === 0) {
+      return new Response("存在しないトレーニング種目です", { status: 404 });
+    }
+    if (error) {
+      return new Response("サーバーエラーが発生しました", { status: 500 });
+    }
+
+    return new Response("トレーニング種目が更新されました", { status: 200 });
+  } catch (err) {
+    return new Response("サーバーエラーが発生しました", { status: 500 });
+  }
+}
