6b102152b5411bf0d1d28d3e6122fe59e98929f0
Yurika Hokama <yurihoka@gmail.com>
2024-12-29 22:02:05 +0900

    [UI] Buttonの横幅サイズ修正

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -3,34 +3,35 @@ import { ButtonProps } from "./types";
 
 const Button = ({ entry, onClick }: ButtonProps) => {
   const className =
-    "w-full bg-lime-300 hover:bg-lime-200 text-gray-700 py-2 px-8 rounded";
-
+    "w-full py-3 px-4 text-sm tracking-wide rounded-lg text-white bg-lime-400 hover:bg-lime-300 focus:outline-none";
   return (
     <div>
-      {entry === "signin" && <button className={className}>SIGN IN</button>}
-      {entry === "signup" && <button className={className}>SIGN UP</button>}
-      {entry === "startworkout" && (
-        <button className={className}>START WORKOUT</button>
-      )}
-      {entry === "finishworkout" && (
-        <button className={className}>FINISH WORKOUT</button>
-      )}
-      {entry === "addset" && (
-        <button className={className} onClick={onClick}>
-          ADD SET
-        </button>
-      )}
-      {entry === "addexercises" && (
-        <button className={className}>ADD EXERCISES</button>
-      )}
-      {entry === "cancelworkout" && (
-        <button className={className}>CANCEL WORKOUT</button>
-      )}
-      {entry === "closeexerciselist" && (
-        <button className={className} onClick={onClick}>
-          X
-        </button>
-      )}
+      <div className="max-w-md w-full">
+        {entry === "signin" && <button className={className}>SIGN IN</button>}
+        {entry === "signup" && <button className={className}>SIGN UP</button>}
+        {entry === "startworkout" && (
+          <button className={className}>START WORKOUT</button>
+        )}
+        {entry === "finishworkout" && (
+          <button className={className}>FINISH WORKOUT</button>
+        )}
+        {entry === "addset" && (
+          <button className={className} onClick={onClick}>
+            ADD SET
+          </button>
+        )}
+        {entry === "addexercises" && (
+          <button className={className}>ADD EXERCISES</button>
+        )}
+        {entry === "cancelworkout" && (
+          <button className={className}>CANCEL WORKOUT</button>
+        )}
+        {entry === "closeexerciselist" && (
+          <button className={className} onClick={onClick}>
+            X
+          </button>
+        )}
+      </div>
     </div>
   );
 };
