f11e56959c6cad68a18633f0d8799cf92cfb9e6c
Yurika Hokama <yurihoka@gmail.com>
2024-12-29 22:03:51 +0900

    [UI] Buttonのマージン追加

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -7,30 +7,32 @@ const Button = ({ entry, onClick }: ButtonProps) => {
   return (
     <div>
       <div className="max-w-md w-full">
-        {entry === "signin" && <button className={className}>SIGN IN</button>}
-        {entry === "signup" && <button className={className}>SIGN UP</button>}
-        {entry === "startworkout" && (
-          <button className={className}>START WORKOUT</button>
-        )}
-        {entry === "finishworkout" && (
-          <button className={className}>FINISH WORKOUT</button>
-        )}
-        {entry === "addset" && (
-          <button className={className} onClick={onClick}>
-            ADD SET
-          </button>
-        )}
-        {entry === "addexercises" && (
-          <button className={className}>ADD EXERCISES</button>
-        )}
-        {entry === "cancelworkout" && (
-          <button className={className}>CANCEL WORKOUT</button>
-        )}
-        {entry === "closeexerciselist" && (
-          <button className={className} onClick={onClick}>
-            X
-          </button>
-        )}
+        <div className="!mt-8">
+          {entry === "signin" && <button className={className}>SIGN IN</button>}
+          {entry === "signup" && <button className={className}>SIGN UP</button>}
+          {entry === "startworkout" && (
+            <button className={className}>START WORKOUT</button>
+          )}
+          {entry === "finishworkout" && (
+            <button className={className}>FINISH WORKOUT</button>
+          )}
+          {entry === "addset" && (
+            <button className={className} onClick={onClick}>
+              ADD SET
+            </button>
+          )}
+          {entry === "addexercises" && (
+            <button className={className}>ADD EXERCISES</button>
+          )}
+          {entry === "cancelworkout" && (
+            <button className={className}>CANCEL WORKOUT</button>
+          )}
+          {entry === "closeexerciselist" && (
+            <button className={className} onClick={onClick}>
+              X
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
