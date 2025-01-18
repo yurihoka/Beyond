ca752aab58e62653673234442beb147880a0aa68
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2025-01-18 05:17:09 -0800

    [Feat] Exercises API 追加 (#66)
    * add: exerciseルート
    
    * add: /exercises GETテスト
    
    * update: /exercises GETテスト修正
    
    * add: /exercise GET DB連携
    
    * add: /exercise POSTテスト
    
    * add: exercises API トレーニング種目追加
    
    * add: /exercises PATCHテスト
    
    * add: /exercises API トレーニング種目更新
    
    * add: /exercises DELETEテスト
    
    * add: /exercises API トレーニング種目削除
    
    * chore: remove unnecessary console.log

---
+++ src/app/api/exercises/route.ts
@@ -0,0 +1,93 @@
+import { createClient } from "@supabase/supabase-js";
+import { type NextApiRequest } from "next";
+
+export async function GET() {
+  try {
+    const client = createClient(
+      process.env.SUPABASE_URL as string,
+      process.env.SUPABASE_ANON_KEY as string
+    );
+    const { data, error } = await client.from("exercises").select("name");
+
+    if (error) throw error;
+
+    return new Response(JSON.stringify(data));
+  } catch (err) {
+    console.log(err);
+    return new Response("サーバーエラーが発生しました", { status: 500 });
+  }
+}
+export async function POST(req: NextApiRequest) {
+  const newExercise = req.body;
+
+  try {
+    const client = createClient(
+      process.env.SUPABASE_URL as string,
+      process.env.SUPABASE_ANON_KEY as string
+    );
+    const { error } = await client.from("exercises").insert(newExercise);
+
+    if (error) {
+      if (error.code === "23505")
+        return new Response("既に存在するトレーニング種目です", {
+          status: 409,
+        });
+      return new Response("サーバーエラーが発生しました", { status: 500 });
+    }
+
+    return new Response("トレーニング種目が追加されました", { status: 201 });
+  } catch (err) {
+    return new Response("サーバーエラーが発生しました", { status: 500 });
+  }
+}
+
+export async function PATCH(req: NextApiRequest) {
+  try {
+    const client = createClient(
+      process.env.SUPABASE_URL as string,
+      process.env.SUPABASE_ANON_KEY as string
+    );
+    const { data, error } = await client
+      .from("exercises")
+      .update({ name: req.body.name })
+      .eq("id", req.body.id)
+      .select();
+
+    if (error?.code === "23505")
+      return new Response("既に存在するトレーニング種目です", {
+        status: 409,
+      });
+    if (data?.length === 0) {
+      return new Response("存在しないトレーニング種目です", { status: 404 });
+    }
+    if (error) {
+      return new Response("サーバーエラーが発生しました", { status: 500 });
+    }
+
+    return new Response("トレーニング種目が更新されました", { status: 200 });
+  } catch (err) {
+    return new Response("サーバーエラーが発生しました", { status: 500 });
+  }
+}
+
+export async function DELETE(req: NextApiRequest) {
+  try {
+    const id = req.body.id;
+    const client = createClient(
+      process.env.SUPABASE_URL as string,
+      process.env.SUPABASE_ANON_KEY as string
+    );
+    const { data, error } = await client
+      .from("exercises")
+      .delete()
+      .eq("id", id)
+      .select();
+
+    if (data?.length === 0) {
+      return new Response("存在しないトレーニング種目です", { status: 404 });
+    }
+    return new Response("トレーニング種目が削除されました", { status: 204 });
+  } catch (err) {
+    return new Response("サーバーエラーが発生しました", { status: 500 });
+  }
+}
