a3c22522a406e074bd21a4f25bb76bfdea676bca
HidemichiShimura <hidemichi.shimura@gmail.com>
2025-01-19 15:28:22 +0900

    add: サインアップページ追加

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Form from "@/components/common/Form";
+
+const Page: NextPage = () => {
+  return (
+    <>
+      <Form entry="signup" />
+    </>
+  );
+};
+
+export default Page;
