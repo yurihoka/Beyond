f1388d81083f5f8d020ca39d3be5ace35dd6ca80
HidemichiShimura <hidemichi.shimura@gmail.com>
2025-01-19 13:34:27 +0900

    add: 全コンポーネントをエントリーポイントに追加

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,16 @@
 /*--- common ---*/
+export { default as Button } from "./common/Button";
+export { default as Form } from "./common/Form";
+export { default as Input } from "./common/Input";
+export { default as Navbar } from "./common/Navbar";
 
 /*--- dashboard ---*/
-
-/*--- exercise ---*/
+export { default as Dashboard } from "./dashboard/Dashboard";
 
 /*--- workout ---*/
+export { default as Exercise } from "./workout/Exercise";
+export { default as ExerciseList } from "./workout/ExerciseList";
+export { default as ExerciseName } from "./workout/ExerciseName";
+export { default as RecordInput } from "./workout/RecordInput";
+export { default as Workout } from "./workout/Workout";
+export { default as WorkoutRecord } from "./workout/WorkoutRecord";
