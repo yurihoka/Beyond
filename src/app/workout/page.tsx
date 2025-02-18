9bb7944ad12d842303e3e61e7b15a4a79a370f03
Yurika Hokama <125883144+yurihoka@users.noreply.github.com>
2025-02-18 03:18:03 -0800

    ワークアウトページコンポーネント作成 (#85)
    * ワークアウトページコンポーネント作成
    
    * components/workoutは不要なため削除
    
    * workoutコンポーネントからlayoutコンポーネントにnavbarコンポーネントを移動
    
    * workoutコンポーネントの内容をpageコンポーネントに移動

--- src/app/workout/page.tsx
+++ src/app/workout/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import type { NextPage } from "next";
 import React, { useEffect, useState } from "react";
-import { Navbar, Button, Exercise } from "@/components";
-import { redirect } from "next/navigation";
+import { Button, Exercise } from "@/components";
+import { useRouter } from "next/navigation";
 
-const Workout = () => {
+const Page: NextPage = () => {
   const [currentDate, setCurrentDate] = useState<string>("");
+  const router = useRouter();
+  const onClick = () => router.push("/dashboard");
 
   useEffect(() => {
     const date = new Date();
@@ -13,24 +16,16 @@ const Workout = () => {
   }, []);
 
   return (
-    <div className="relative">
-      <div className="fixed top-0 left-0 w-full">
-        <Navbar userEmail="test@xyz.com" />
-      </div>
+    <div className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg mx-auto">
       <div className="flex pt-10 justify-between justify-center items-center">
         <h1>{currentDate}</h1>
         <Button entry="finishworkout" />
       </div>
       <Exercise />
       <Button entry="addexercises" />
-      <Button
-        entry="cancelworkout"
-        onClick={() => {
-          redirect(`/dashboard`);
-        }}
-      />
+      <Button entry="cancelworkout" onClick={onClick} />
     </div>
   );
 };
 
-export default Workout;
+export default Page;
