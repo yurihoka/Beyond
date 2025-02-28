fc64459c7f8512f78802ca87b9ca154325715b67
HidemichiShimura <hidemichi.shimura@gmail.com>
2025-02-28 17:14:29 +0900

    fix: ナビゲーションバーのスタイル修正

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -8,13 +8,13 @@ export type NavbarProps = {
 const Navbar = ({ userEmail }: NavbarProps) => {
   return (
     <div>
-      <ul className="flex justify-between bg-gray-200 px-10">
+      <ul className="flex justify-between items-center bg-gray-200 px-10">
         <li className="mr-3">
           <a className="text-gray-500 hover:text-gray-700" href="#">
             HOME
           </a>
         </li>
-        <li className="mr-3">
+        <li className="mr-3 flex items-center gap-4">
           <SignoutButton />
           <a className="text-gray-500 hover:text-gray-700" href="#">
             {userEmail}
