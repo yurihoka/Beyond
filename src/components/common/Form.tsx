3c53726139e8fcc140ba064c9ed683b79d153d47
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2025-02-28 09:27:20 +0900

    [Feat] ログイン認証後のアプリ全体でのユーザ情報共有 (#93)
    * add: ログインユーザーコンテクストでアプリ全体をラップ
    
    * fix: サインアップフォームのインプット値をstateで管理
    
    * add: ログイン認証後のページ遷移とログインユーザ情報反映
    
    * fix: ナビゲーションバーにログインユーザのemailを表示
    
    * fix: ページ更新時のログインユーザ情報削除

--- src/components/common/Form.tsx
+++ src/components/common/Form.tsx
@@ -1,12 +1,35 @@
 import React from "react";
 import { Input, Button } from "@/components";
-import { ButtonProps } from "./types";
 
-const Form = ({ entry, onClick }: ButtonProps) => {
+type FormProps = {
+  entry:
+    | "signin"
+    | "signup"
+    | "startworkout"
+    | "finishworkout"
+    | "addset"
+    | "addexercises"
+    | "cancelworkout"
+    | "closeexerciselist";
+  onClick: (e: React.MouseEvent<HTMLInputElement>) => void;
+  email: string;
+  setEmail: React.Dispatch<React.SetStateAction<string>>;
+  pswd: string;
+  setPswd: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const Form = ({
+  entry,
+  onClick,
+  email,
+  setEmail,
+  pswd,
+  setPswd,
+}: FormProps) => {
   return (
     <div>
-      <Input variant="Email" />
-      <Input variant="Password" />
+      <Input variant="Email" value={email} setValue={setEmail} />
+      <Input variant="Password" value={pswd} setValue={setPswd} />
       <Button entry={entry} onClick={onClick} />
     </div>
   );
