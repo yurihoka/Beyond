0172a3958c69d0bd0960e4be70fef01aa7ba720e
Yurika Hokama <125883144+yurihoka@users.noreply.github.com>
2025-04-02 17:29:56 +0900

    [Feat] Workoutページ内のトレーニング種目追加ボタン実装 (#102)
    * addボタン追加
    
    * Formコンポーネントが画面の中央に表示されるように修正 (#104)
    
    * Formコンポーネントが画面の中央に表示されるように修正
    
    * fix: サインイン/サインアウトフォームを画面中央へ配置
    
    ---------
    
    Co-authored-by: HidemichiShimura <hidemichi.shimura@gmail.com>
    
    * [Feat] 非認証ユーザのユーザページアクセス時のサインインページ強制リダイレクト (#106)
    
    * add: 認証なしユーザの強制リダイレクト用のコンポーネント追加
    
    * add: 非認証ユーザのリダイレクトをユーザページへ追加
    
    * buttonコンポーネント内にonClick追加 (#108)
    
    * [Feat] Signupページでの新規ユーザ登録処理追加 (#113)
    
    * add: /signupページとsignup APIの連携処理追加
    
    * fix: SignupページのSignupボタンの表示文字修正
    
    * [Chore] ユーザ認証/専用ページ用のRoute Groups作成 (#110)
    
    * add: ユーザ認証ページのroute group作成
    
    * add: ユーザ専用ページのroute group作成
    
    * add: /signup /signinの共通レイアウト作成
    
    * add: ユーザ専用ページの共通レイアウト作成
    
    * [Feat] サインインページ内にサインアップページへのリンクを追加 (#115)
    
    * del: 不要なマージメッセージを削除
    
    * add: サインアップページへのリンクボタンをサインインページへ配置
    
    * del: Wokroutページの不要なスタイルを削除
    
    * chore: <Exercise />の種目名を表示
    
    * chore: Workoutページのポップアップ用のステート名を変更
    
    * del: Workoutページコンポーネントの不要なimportを削除
    
    * add: Workoutページのポップアップからトレーニング記録に新規種目を追加
    
    * del: <ExerciseListPopup />の未使用import削除
    
    * add: WorkoutページのポップアップにAPIコールで取得した種目を表示
    
    ---------
    
    Co-authored-by: HidemichiShimura <hidemichi.shimura@gmail.com>

--- src/app/(user)/workout/page.tsx
+++ src/app/(user)/workout/page.tsx
@@ -2,17 +2,13 @@
 
 import type { NextPage } from "next";
 import React, { useEffect, useState } from "react";
-import {
-  Button,
-  Exercise,
-  ExerciseListPopUp,
-  NoAuthUserRedirection,
-} from "@/components";
+import { Button, Exercise, ExerciseListPopUp } from "@/components";
 import { useRouter } from "next/navigation";
 
 const Page: NextPage = () => {
   const [currentDate, setCurrentDate] = useState<string>("");
-  const [isOpen, setIsOpen] = useState(false);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [exercises, setExercises] = useState<string[]>([]);
   const router = useRouter();
   const onClick = () => router.push("/dashboard");
 
@@ -23,19 +19,22 @@ const Page: NextPage = () => {
 
   return (
     <>
-      {isOpen && (
+      {isPopupOpen && (
         <ExerciseListPopUp
-          onCancel={() => setIsOpen(false)}
-          onOk={() => setIsOpen(false)}
+          setExercises={setExercises}
+          onCancelClick={() => setIsPopupOpen(false)}
+          onAddClick={() => setIsPopupOpen(false)}
         />
       )}
       <div className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg mx-auto">
-        <div className="flex pt-10 justify-between justify-center items-center">
+        <div className="flex pt-10 justify-between items-center">
           <h1>{currentDate}</h1>
           <Button entry="finishworkout" />
         </div>
-        <Exercise />
-        <Button entry="addexercises" onClick={() => setIsOpen(true)} />
+        {exercises.map((exercise, index) => (
+          <Exercise key={index} name={exercise} />
+        ))}
+        <Button entry="addexercises" onClick={() => setIsPopupOpen(true)} />
         <Button entry="cancelworkout" onClick={onClick} />
       </div>
     </>
