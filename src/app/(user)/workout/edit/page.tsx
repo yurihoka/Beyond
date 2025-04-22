5248b10152a527600cd5fc97c0f0c954302f633b
HidemichiShimura <hidemichi.shimura@gmail.com>
2025-04-23 01:05:23 +0900

    add: ログインユーザの過去トレーニング日付をドロップダウン表示

--- src/app/(user)/workout/edit/page.tsx
+++ src/app/(user)/workout/edit/page.tsx
@@ -1,7 +1,52 @@
+"use client";
+
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+
+type WorkoutSet = { rep: number; weight: number };
+type workoutHistory = {
+  date: string;
+  data: {
+    name: string;
+    sets: WorkoutSet[];
+  }[];
+};
 
 const Page: NextPage = () => {
-  return <></>;
+  const [workoutHistories, setWorkoutHistories] = useState<workoutHistory[]>(
+    []
+  );
+
+  useEffect(() => {
+    const fetchWorkoutHistories = async () => {
+      const res = await fetch(
+        `${
+          process.env.NEXT_PUBLIC_HOST
+        }/api/histories?email=${localStorage.getItem("email")}`
+      );
+      const data = await res.json();
+      const histories = data[0].data;
+
+      console.log(histories);
+
+      setWorkoutHistories(histories);
+    };
+
+    fetchWorkoutHistories();
+  }, []);
+
+  return (
+    <>
+      <label htmlFor="dates">日付</label>
+      <select name="dates">
+        {workoutHistories.map((history) => (
+          <option value={history.date} key={history.date}>
+            {history.date}
+          </option>
+        ))}
+      </select>
+    </>
+  );
 };
 
 export default Page;
