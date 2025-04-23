1c29d88df26bb5b3ec4a87972da087ff5d206fa1
HidemichiShimura <hidemichi.shimura@gmail.com>
2025-04-24 08:42:38 +0900

    fix: デプロイ環境のcorsエラー解消

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@ const nextConfig = {
   async redirects() {
     return [{ source: "/", destination: "/signin", permanent: true }];
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
